e473caf3a5144e1dccc7393d2d2698eab6342102
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2017-11-23 14:36:52 +0700

    pendingIntent

--- app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
+++ app/src/main/java/com/example/android/mygarden/PlantWidgetProvider.java
@@ -1,23 +1,25 @@
 package com.example.android.mygarden;
 
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
-/**
- * Implementation of App Widget functionality.
- */
-public class PlantWidgetProvider extends AppWidgetProvider {
+import com.example.android.mygarden.ui.MainActivity;
 
+public class PlantWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Create an Intent to launch MainActivity when clicked
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.plant_widget);
+        // Widgets allow click handlers to only launch pending intents
+        views.setOnClickPendingIntent(R.id.widget_plant_image, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -31,13 +33,18 @@ public class PlantWidgetProvider extends AppWidgetProvider {
     }
 
     @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // Perform any action when one or more AppWidget instances have been deleted
+    }
+
+    @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+        // Perform any action when an AppWidget for this provider is instantiated
     }
 
     @Override
     public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+        // Perform any action when the last AppWidget instance for this provider is deleted
     }
-}
 
+}
